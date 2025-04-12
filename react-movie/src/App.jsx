86c95c0cb671c0353a2159da889283c7884e96f4
mortalprotagonist <aktr2003@gmail.com>
2025-04-13 01:45:33 +0530

    Added styling in css folder and added Navbar component

--- react-movie/src/App.jsx
+++ react-movie/src/App.jsx
@@ -1,20 +1,23 @@
 
-import './App.css'
+import './css/App.css'
 import Home from './pages/Home'
 import Favorites from './pages/Favorites'
-import { Route,Routes } from 'react-router-dom'
-
+import { Route, Routes } from 'react-router-dom'
+import NavBar from './components/NavBar';
 
 function App() {
- 
+
 
   return (
-   <main classaName="main-content">
-    <Routes>
-      <Route path="/" element={<Home/>}></Route>
-      <Route path="/favorites" element={<Favorites/>}></Route>
-    </Routes>
-   </main>
+    <div>
+      <NavBar/>
+      <main classaName="main-content">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/favorites" element={<Favorites />}></Route>
+        </Routes>
+      </main>
+    </div>
   )
 }
 
---
+++ react-movie/src/components/NavBar.jsx
@@ -0,0 +1,18 @@
+import "../css/Navbar.css"
+import { Link } from "react-router-dom";
+
+function NavBar(){
+    return(
+        <nav className="navbar">
+            <div className="navbar-brand">
+                <Link to="/">Movie App</Link>
+            </div>
+            <div className="navbar-links">
+                <Link to="/">Home</Link>
+                <Link to="/favorites">Favorites</Link>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
