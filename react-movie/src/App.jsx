ca3886e88ba909e8d10c976982dd9a22401e1984
mortalprotagonist <aktr2003@gmail.com>
2025-04-19 00:52:19 +0530

    added changes

--- react-movie/src/App.jsx
+++ react-movie/src/App.jsx
@@ -1,27 +1,22 @@
-
-import './css/App.css'
-import Home from './pages/Home'
-import Favorites from './pages/Favorites'
-import { Route, Routes } from 'react-router-dom'
-import NavBar from './components/NavBar';
-import { MovieProvider } from './contexts/MovieContext';
-
+import "./css/App.css";
+import Favorites from "./pages/Favorites";
+import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
+import { MovieProvider } from "./contexts/MovieContext";
+import NavBar from "./components/NavBar";
 
 function App() {
-
-
   return (
-      <MovieProvider>
-      <NavBar/>
+    <MovieProvider>
+      <NavBar />
       <main className="main-content">
         <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/favorites" element={<Favorites />}></Route>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
         </Routes>
       </main>
-      </MovieProvider>
-  )
+    </MovieProvider>
+  );
 }
 
-
-export default App
+export default App;
--- react-movie/src/contexts/MovieContext.jsx
+++ react-movie/src/contexts/MovieContext.jsx
@@ -1,33 +1,35 @@
- import { createContext,useContext, useState, useEffect } from "react";
+import {createContext, useState, useContext, useEffect} from "react"
 
- const MovieContext= createContext()
+const MovieContext = createContext()
 
- export const useMovieContext = () => useContext(MovieContext)
+export const useMovieContext = () => useContext(MovieContext)
 
- export const MovieProvider = ({children}) => {
-    const [favorites, setFavorites] = ([])
+export const MovieProvider = ({children}) => {
+    const [favorites, setFavorites] = useState([])
 
     useEffect(() => {
-        const storedFavs= localStorage.getItem("favorites");
+        const storedFavs = localStorage.getItem("favorites")
+
         if (storedFavs) setFavorites(JSON.parse(storedFavs))
     }, [])
+
     useEffect(() => {
-        localStorage.setItem('favorites',JSON.stringify(favorites))
+        localStorage.setItem('favorites', JSON.stringify(favorites))
     }, [favorites])
-    const addToFavorites =(movie) => {
-        setFavorites(prev => [...prev,movie])
+
+    const addToFavorites = (movie) => {
+        setFavorites(prev => [...prev, movie])
     }
 
     const removeFromFavorites = (movieId) => {
         setFavorites(prev => prev.filter(movie => movie.id !== movieId))
     }
-
-    const isFavorite = (movieId)=>{
+    
+    const isFavorite = (movieId) => {
         return favorites.some(movie => movie.id === movieId)
     }
 
     const value = {
-
         favorites,
         addToFavorites,
         removeFromFavorites,
@@ -37,4 +39,4 @@
     return <MovieContext.Provider value={value}>
         {children}
     </MovieContext.Provider>
- }
+}
